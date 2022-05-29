ffa7eab61dbae6f664c0c21a8ec84a2c7541344e
nepalsandesh <sandeshnepal43@gmail.com>
2022-05-30 00:22:29 +0545

    Automatic configuration with django-heroku

--- Apiculture_2/settings.py
+++ Apiculture_2/settings.py
@@ -129,3 +129,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'MainApp/media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import django_heroku
+django_heroku.settings(locals())
+import django_heroku
+django_heroku.settings(locals())
